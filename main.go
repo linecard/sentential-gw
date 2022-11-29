16ee18d55b65e2eadafb6398858d9b84a42ecfca
Raymond Douglas <r@rymnd.org>
2022-11-28 20:50:41 -0800

    embed version

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/wheegee/sentential-gw/internal/proxy"
 	"github.com/wheegee/sentential-gw/internal/util"
 )
 
+var version = "dev" // Set by goreleaser.
+
 func main() {
-	util.Log.Info("starting gateway", "port", util.Port)
+	util.Log.Info("starting gateway", "version", version, "port", util.Port)
 	http.HandleFunc("/", proxy.Handle)
-	http.ListenAndServe(util.Port, nil)
+	if err := http.ListenAndServe(util.Port, nil); err != nil {
+		util.Log.Error("error starting exporter", "error", err)
+		os.Exit(1)
+	}
 }
