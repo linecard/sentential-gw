a6b7865eeb285d0adf454c5722c76a9a5c4bd09f
Raymond Douglas <r@rymnd.org>
2023-09-28 23:15:04 +0100

    extra flaké

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs= {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    sntlGw.url = "github:linecard/sentential-gw/flake";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, sntlGw, ... }:
   let
     system = "aarch64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -9,10 +12,11 @@
     sententialGw = pkgs.stdenv.mkDerivation {
       __noChroot = true;
       name = "sentential-gw";
-      src = ./src;
+      src = sntlGw;
       buildInputs = [ pkgs.go_1_21 ];
 
       buildPhase = ''
+        cd src
         export HOME=$TMPDIR
         mkdir -p $out/bin
         go build -o $out/bin/sntl-gw
